bbbb6d9447e89adcb7fa317ee50051755d3aec89
Jalal <mohd.farhan.jalal@intel.com>
2018-01-06 15:42:00 +0800

    enable google api

--- App.js
+++ App.js
@@ -13,6 +13,7 @@ import {
 } from 'react-native';
 import Camera from 'react-native-camera';
 import { error } from 'util';
+import axios from 'axios';
 
 
 export default class App extends Component<{}> {
@@ -20,7 +21,7 @@ export default class App extends Component<{}> {
   constructor(){
     super();
     this.state = {
-      sampleText = 'Test'
+      sampleText : 'Test'
     };
   }
   render() {
@@ -30,8 +31,13 @@ export default class App extends Component<{}> {
           ref={(cam)=>{
             this.camera = cam
           }}
+          captureQuality={Camera.constants.CaptureQuality['480p']}
+          captureTarget={Camera.constants.CaptureTarget.memory}
+          playSoundOnCapture={false}
+          jpegQuality={40}
           style = {styles.view}
           aspect = {Camera.constants.Aspect.fill}>
+          <Text style={{color:'blue'}}>{this.state.sampleText}</Text>
           <Text
             style = {styles.capture}
             onPress = {this.takePicture.bind(this)}
@@ -44,14 +50,79 @@ export default class App extends Component<{}> {
   }
 
   takePicture(){
+
     const options = {};
 
-    this.camera.capture({metadata: options}).then((data)=>{
-      console.log(data);
-    }).catch((error)=>{
+    const cloudVision = "https://vision.googleapis.com/v1/images:annotate?key=AIzaSyA_gtgnukVcMq7YxkpYtbPBu97fldIACjY";
+
+    this.camera.capture({metadata:options}).then((data)=>{
+      //console.log(data);
+      // this.setState({
+      //   //sampleText: data,
+      //   sampleText: 'Hello World'
+      // });
+
+      axios.post(cloudVision, {
+        requests: [
+          {
+            image: {
+              content:data.data,
+            },
+            features: [{
+              type: 'LOGO_DETECTION',
+              maxResults: 1,
+            }],
+          },
+        ],
+      }).then(function(response){
+
+        const self = this;
+        if(response.data.responses[0].logoAnnotations[0].description){
+          this.setState({
+            //sampleText: 'Hello World',
+            sampleText: response.data.responses[0].logoAnnotations[0].description
+          });
+        }
+      
+        //console.log(response.data.responses[0].textAnnotations[0].description);
+        console.log(response.data.responses[0].logoAnnotations[0].description);
+      }.bind(this)).catch(function(error){
+        console.log(error);
+      });
+    }).catch(function(error){
       console.log(error);
-    })
+    });
   }
+  // async takePicture(){
+
+  //   const cloudVision = "https://vision.googleapis.com/v1/images:annotate?key=AIzaSyA_gtgnukVcMq7YxkpYtbPBu97fldIACjY";
+  //   const options = {};
+
+  //   try{
+  //     const image64 = await this.camera.capture();
+
+  //     const response = await axios.post(cloudVision, {
+  //       requests: [
+  //         {
+  //           image: {
+  //             content: image64.data,
+  //           },
+  //           features: [{
+  //             type: 'TEXT_DETECTION',
+  //             maxResults: 1,
+  //           }],
+  //         },
+  //       ],
+  //     });
+
+  //     console.log(response.data.responses[0].textAnnotations);
+  //   }catch(ex){
+
+  //   }
+  
+
+   
+  // }
 }
 
 const styles = StyleSheet.create({
